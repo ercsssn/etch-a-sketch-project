8d6c872f7563368887d787dfb24e185d2b6fea5b
ercsssn <ercsssn@gmail.com>
2022-03-16 17:04:34 +0800

    Create a grid
    This is a 35x35 grid container of 16px grid boxes.

---
+++ script.js
@@ -0,0 +1,11 @@
+let gridContainer = document.querySelector('.container')
+
+for (let i = 1; i <= 1225; i++) {
+    let gridBox = document.createElement('div');
+    gridBox.style.cssText = 'background: black; height:16px; width:16px;';
+    gridContainer.appendChild(gridBox);
+}
+
+
+
+
