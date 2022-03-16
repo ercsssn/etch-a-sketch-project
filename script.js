2e7dc794f77cd09c398a8167d016fe09176f87b5
ercsssn <ercsssn@gmail.com>
2022-03-17 00:06:39 +0800

    Add a reset button functionality
    This will ask the prompt again to replace the existing grid by a new grid according to the new prompt.

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-let gridContainer = document.querySelector('.container');
-let boxPerSide;
+function createGrid(e) {
 
-while (true) {
-    let popup = prompt('Enter number of grid box per side:');
-    boxPerSide = parseInt(popup);
-
-    if (boxPerSide > 100) {
-        alert('Too large');
-        continue;
-    }else if (boxPerSide < 0) {
-        alert('Enter a valid number');
-        continue;
-    }else if (boxPerSide > 0 && boxPerSide < 100){
-        break;
-    } else {
-        alert('Enter a valid input');
+    if (boxPerSide > 0) {
+        let grid = gridContainer.querySelectorAll('div');
+        grid.forEach(elem => elem.remove());
     }
-}
+    
+    while (true) {
+        let popUp = prompt('Enter number of grid box per side:');
+        boxPerSide = parseInt(popUp);
+    
+        if (boxPerSide > 100) {
+            alert('Too large');
+            continue;
+        }else if (boxPerSide < 0) {
+            alert('Enter a valid number');
+            continue;
+        }else if (boxPerSide > 0 && boxPerSide < 100){
+            break;
+        } else {
+            alert('Enter a valid input');
+        }
+    }
 
-for (let i = 1; i <= boxPerSide*boxPerSide; i++) {                 //This for loop will create the grid
-    let gridBox = document.createElement('div');
-    gridBox.setAttribute('class', 'box');
-    gridBox.style.cssText = `height:${560/boxPerSide}px; width:${560/boxPerSide}px;`;
-    gridContainer.appendChild(gridBox);
+    for (let i = 1; i <= boxPerSide*boxPerSide; i++) {      //This for loop will create the grid
+        console.log(boxPerSide);
+        let gridBox = document.createElement('div');
+        gridBox.setAttribute('class', 'box');
+        gridBox.style.cssText = `height:${560/boxPerSide}px; width:${560/boxPerSide}px;`; //560 refers to the container display size
+        gridContainer.appendChild(gridBox);
+    }
+
+    let allBox = gridContainer.querySelectorAll('div');
+    allBox.forEach(box => box.addEventListener('mouseover', changeColor));
 }
 
 function changeColor(e) {
+    console.log(e.target);
     let oneBox = e.target;
     oneBox.classList.add('boxhover');
 }
 
-let allBox = gridContainer.querySelectorAll('div');
-allBox.forEach(box => box.addEventListener('mouseover', changeColor));
+let gridContainer = document.querySelector('.container');
 
+let boxPerSide;
 
+let resetButton = document.querySelector('.title button');
+resetButton.addEventListener('click', createGrid);
 
 
