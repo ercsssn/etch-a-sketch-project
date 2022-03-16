820763279807639ea73a8e2e8ed404c892494a24
ercsssn <ercsssn@gmail.com>
2022-03-16 18:29:52 +0800

    Add mouseover event listener to each grid box
    This will change color of the grid box when the cursor hover on it.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let gridContainer = document.querySelector('.container')
 
-for (let i = 1; i <= 1225; i++) {
+for (let i = 1; i <= 1225; i++) {                 //This for loop will create the grid
     let gridBox = document.createElement('div');
-    gridBox.style.cssText = 'background: black; height:16px; width:16px;';
+    gridBox.setAttribute('class', 'box');
+    gridBox.style.cssText = 'height:16px; width:16px;';
     gridContainer.appendChild(gridBox);
 }
+
+function changeColor(e) {
+    let oneBox = e.target;
+    oneBox.classList.add('boxhover');
+}
+
+let allBox = gridContainer.querySelectorAll('div');
+allBox.forEach(box => box.addEventListener('mouseover', changeColor));
 
 
 
